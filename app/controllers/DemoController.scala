ad2a13684160f088a7c19b6227bc686f3310ef8d
Etienne ANNE <etienne.anne@icloud.com>
2020-12-10 09:31:03 +0100

    Ajout des endpoints pour gérer les utilisateurs

--- app/controllers/DemoController.scala
+++ app/controllers/DemoController.scala
@@ -1,16 +1,21 @@
 package controllers
 
 import play.api.data.validation.ValidationError
-import play.api.libs.json.Json
+import play.api.libs.json._
 import play.api.mvc._
 
 import javax.inject.{Inject, Singleton}
-import scala.collection.immutable.HashMap
+import scala.collection.mutable
 
 case class User(id: Int, name: String, firstName: String)
 
+object User {
+  implicit val userToJson = Json.format[User]
+  implicit val userReads = Json.reads[User]
+}
+
 object Datastore {
-  val users: HashMap[Int, User] = HashMap()
+  val users: mutable.HashMap[Int, User] = mutable.HashMap()
 
   def get(id: Int): Option[User] = users get id
 
@@ -18,20 +23,20 @@ object Datastore {
     users get id match {
       case Some(_) => Left(ValidationError("user already existed"))
       case _ =>
-        users += (id, data)
+        users += id -> data
         Right(data)
     }
   }
   def update(id: Int, data: User): Either[ValidationError, User] = {
     users get id match {
       case Some(_) =>
-          users += (id, data)
+          users += id -> data
           Right(data)
       case _ => Left(ValidationError("user not found"))
     }
   }
-  def delete(id: Int, data: User): Unit = {
-    users -= (id, data)
+  def delete(id: Int): Unit = {
+    users -= id
   }
 }
 
@@ -39,30 +44,57 @@ object Datastore {
 class DemoController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController {
 
-  def greeting(msg: String) = Action {
+  def index(): Action[AnyContent] = Action {
     Ok(Json.obj(
-      "message" -> msg
+      "users" -> Datastore.users
     ))
   }
 
-  def index() = Action {
-    
+  def create(): Action[AnyContent] = Action { request =>
+    request.body.asJson match {
+      case Some(value) =>
+        val userFromJson: JsResult[User] = Json.fromJson[User] (value)
+        userFromJson match {
+          case e @ JsError(_) => Results.BadRequest(JsError.toJson(e).toString())
+          case JsSuccess(u: User, _) =>
+            Datastore.create (u.id, u) match {
+              case Left(value) => Results.BadRequest(value.message)
+              case Right(_) => Redirect("/users")
+            }
+        }
+      case _ => Results.BadRequest("missing body")
+    }
   }
 
-  def create() = Action {
-
+  def get(id: Int): Action[AnyContent] = Action {
+    Datastore.get(id) match {
+      case Some(user) => Ok(Json.toJson(user))
+      case _ => NotFound
+    }
   }
 
-  def get(id: Int) = Action {
-
+  def put(id: Int): Action[AnyContent] = Action { request =>
+    request.body.asJson match {
+      case Some(jsValue) =>
+        val userFromJson: JsResult[User] = Json.fromJson[User](jsValue)
+        userFromJson match {
+          case e @ JsError(_) => Results.BadRequest(JsError.toJson(e).toString())
+          case JsSuccess(u: User, _) =>
+            Datastore.update(id, u) match {
+              case Left(value) => Results.BadRequest(value.message)
+              case Right(_) => Ok("updated")
+            }
+        }
+    }
   }
 
-  def put(id: Int) = Action {
-
-  }
-
-  def delete(id: Int) = Action {
-
+  def delete(id: Int): Action[AnyContent] = Action {
+    val usersSize = Datastore.users.size
+    Datastore.delete(id)
+    if (usersSize == Datastore.users.size)
+      BadRequest("user id not found")
+    else
+      Ok("user removed")
   }
 
 }
